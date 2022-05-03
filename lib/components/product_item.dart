651795f11eec8a854bd0903f14fba46a6be8a537
murilo <murilra@hotmail.com>
2022-05-03 09:07:27 -0300

    primeiro commit

---
+++ lib/components/product_item.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+import '../product.dart';
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+  const ProductItem({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/products_overview_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+
+import '../product.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Minha Loja'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loadedProducts.length,
+          itemBuilder: (ctx, i) => Text(loadedProducts[i].title),
+          // ignore: prefer_const_constructors
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+        ),
+      ),
+    );
+  }
+}
