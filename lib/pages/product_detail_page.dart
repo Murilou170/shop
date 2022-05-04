e74b82636362dfe392ecf0b965ec36c8c57e8e4e
murilo <murilra@hotmail.com>
2022-05-04 17:53:39 -0300

    navegando entre as telas

--- lib/pages/product_detail_page.dart
+++ lib/pages/product_detail_page.dart
@@ -3,18 +3,19 @@ import 'package:flutter/material.dart';
 import '../product.dart';
 
 class ProductDetailPage extends StatelessWidget {
-  
   const ProductDetailPage({
     Key? key,
-    }) : super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final Product product;
+    final Product product =
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Teste'),
+        title: Text(product.title),
       ),
+      
     );
   }
 }
